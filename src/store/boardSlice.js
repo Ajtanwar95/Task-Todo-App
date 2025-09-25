506b0b0106ab60c4c618546f207f57e9a8134458
Ajay Tanwar <ajay.tanwar@molitics.in>
2025-09-25 13:24:29 +0530

    Fix dependency conflict4

--- src/store/boardSlice.js
+++ src/store/boardSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { v4 as uuidv4 } from 'uuid';
 import { ref, set } from 'firebase/database';
-import { database } from '../firebase';    
+import { database } from './firebase';    
 
 const initialState = {
   columns: {},
